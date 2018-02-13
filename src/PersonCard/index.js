7e74c4e3d93b8650e82d6f1143b7a34ec1f92a06
laurarantonen <laurarantonen@hotmail.com>
2018-02-13 14:47:27 +0200

    tyylit

--- src/PersonCard/index.js
+++ src/PersonCard/index.js
@@ -5,9 +5,10 @@ import * as s from './styles.js'
 const PersonCard = (props) => {
   return (
     <s.PersonCard>
+      <img className="img" src={props.imageUrl} />
       <s.Name>{props.name}</s.Name>
       <s.Title>{props.title}</s.Title>
-      <img className="img" src={props.imageUrl} />
+      <s.Description>{props.description}</s.Description>
 
     </s.PersonCard>
   )
