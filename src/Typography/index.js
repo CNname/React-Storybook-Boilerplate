1560b558a2379db19dd78e66c653ed389470436e
Jonne Räsänen <jonne.rasanen@sc5.io>
2018-02-13 13:42:10 +0200

    add Typography

---
+++ src/Typography/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import * as s from './styles.js'
+
+const Typography = (props) => {
+  return (
+    <s.FuturaBold>Hello World!</s.FuturaBold>
+  )
+}
+
+export default Typography
