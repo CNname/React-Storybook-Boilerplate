80519b54ca045fc3ebea441bf5db967618ef0d92
Jonne Räsänen <jonne.rasanen@sc5.io>
2018-02-13 16:00:29 +0200

    add props

--- src/PageTitle/index.js
+++ src/PageTitle/index.js
@@ -5,6 +5,7 @@ import * as s from './styles.js'
 const PageTitle = (props) => {
   return (
     <s.PageTitle>
+      {props.children}
       <s.Title>{props.title}</s.Title>
       <s.Hr />
     </s.PageTitle>
