b4a6778f00dd53e1271e170085e6dab509160af7
Jonne Räsänen <jonne.rasanen@sc5.io>
2018-02-13 14:32:19 +0200

    use theme colors

--- src/PersonCard/styles.js
+++ src/PersonCard/styles.js
@@ -1,4 +1,5 @@
 import styled, { css } from 'styled-components'
+import colors from '../colors';
 
 export const PersonCard = styled.div`
   text-align: center;
@@ -7,7 +8,7 @@ export const PersonCard = styled.div`
 export const Name = styled.h2`
   font-family: futura-pt-bold, sans-serif;
   font-size: 22px;
-  color: #353740;
+  color: ${colors.text.black};
   line-height: 1.4em;
   margin: 0;
 `
@@ -15,7 +16,7 @@ export const Name = styled.h2`
 export const Title = styled.h3`
   font-family: lato, sans-serif;
   font-size: 16px;
-  color: #747d88;
+  color: ${colors.text.grey};
   font-weight: 700;
   margin: 0;
   margin-top: 20px;
